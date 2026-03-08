2506f6aefeda404728d049a87ec59fefda80d42d
velagit <veladavila@upz.edu.mx>
2026-03-07 22:36:57 -0600

    Se creo el primer elemento del repositorio

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple PHP test page.</p>
+    <?php
+        // This is a simple PHP script that outputs the current date and time
+        echo "<p>Current date and time: " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+</body>     
+</html>
